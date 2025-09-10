c9749362d3a441e553c9e350786a08850afb7553
Niklas Färeborg <Fareborg@hotmail.com>
2025-09-10 11:43:19 +0200

    Initial commit integrationsystem

--- IntegrationSystem/Program.cs
+++ IntegrationSystem/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace IntegrationSystem
+﻿using DatabaseCSharp;
+
+namespace IntegrationSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            EasyModbus.ModbusClient modbusClient = new EasyModbus.ModbusClient("127.0.0.1", 502);
+            modbusClient.Connect();
+            //Loopa läsa i databasen - för varje order
+            int orderId = 1; //Hämta från databas
+            modbusClient.WriteSingleRegister(0, orderId);
+
             Console.WriteLine("Hello, World!");
         }
+
     }
 }
